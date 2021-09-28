940e50a4041c10ec6572adfdb844bb56693b7b9e
deopaul <deopaul.somwaru@gmail.com>
2021-09-28 19:11:53 -0400

    - full screen image

--- lib/src/ui/intro_content.dart
+++ lib/src/ui/intro_content.dart
@@ -14,63 +14,61 @@ class IntroContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // return Container(
-    //   margin: page.decoration.contentMargin,
-    //   decoration: isFullScreen
-    //       ? page.decoration.boxDecoration ??
-    //           BoxDecoration(
-    //             color: page.decoration.pageColor,
-    //             borderRadius: BorderRadius.circular(8.0),
-    //           )
-    //       : null,
-
     return Container(
-      child: Stack(
-        children: <Widget>[
-          //image code
-          Image.network(
-            'https://guyanatourism.com/wp-content/uploads/2019/07/Lethem-trail-scaled.jpg',
-          ),
-          //gradient
-          Container(
-            // height: 50,
-            width: double.infinity,
-            decoration: new BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  Colors.black38,
-                  Colors.black26,
-                  // Colors.indigo,
-                ],
-              ),
+      margin: page.decoration.contentMargin,
+      decoration: isFullScreen
+          ? page.decoration.boxDecoration ??
+              BoxDecoration(
+                  image: DecorationImage(
+                      fit: BoxFit.fill,
+                      image: new NetworkImage(
+                          "https://guyanatourism.com/wp-content/uploads/2019/07/Lethem-trail-scaled.jpg")))
+          : null,
+
+      // return Stack(
+      //   children: <Widget>[
+      //     //image code
+      //     Image.network(
+      //       'https://guyanatourism.com/wp-content/uploads/2019/07/Lethem-trail-scaled.jpg',
+      //     ),
+      //     //gradient
+      //     Container(
+      //       // height: 50,
+      //       width: double.infinity,
+      //       decoration: new BoxDecoration(
+      //         gradient: LinearGradient(
+      //           colors: [
+      //             Colors.black38,
+      //             Colors.black26,
+      //             // Colors.indigo,
+      //           ],
+      //         ),
+      //       ),
+      //     ),
+
+      child: Column(
+        children: [
+          Padding(
+            padding: page.decoration.titlePadding,
+            child: _buildWidget(
+              page.titleWidget,
+              page.title,
+              page.decoration.titleTextStyle,
             ),
           ),
-
-          Column(
-            children: [
-              Padding(
-                padding: page.decoration.titlePadding,
-                child: _buildWidget(
-                  page.titleWidget,
-                  page.title,
-                  page.decoration.titleTextStyle,
-                ),
-              ),
-              Padding(
-                padding: page.decoration.descriptionPadding,
-                child: _buildWidget(
-                  page.bodyWidget,
-                  page.body,
-                  page.decoration.bodyTextStyle,
-                ),
-              ),
-              if (page.footer != null)
-                Padding(
-                  padding: page.decoration.footerPadding,
-                  child: page.footer,
-                ),
-            ],
+          Padding(
+            padding: page.decoration.descriptionPadding,
+            child: _buildWidget(
+              page.bodyWidget,
+              page.body,
+              page.decoration.bodyTextStyle,
+            ),
           ),
+          if (page.footer != null)
+            Padding(
+              padding: page.decoration.footerPadding,
+              child: page.footer,
+            ),
         ],
       ),
     );
